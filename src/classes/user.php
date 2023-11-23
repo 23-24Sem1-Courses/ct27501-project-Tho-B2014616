d0c6cd66dc5074a9c9d24ab1cade73ec59616e6f
Tu Ngoc Xuan Tho <thob2014616@student.ctu.edu.vn>
2023-11-23 17:06:22 +0700

    Hoàn thành lớp user

--- src/classes/user.php
+++ src/classes/user.php
@@ -1,8 +1,11 @@
 <?php
-// namespace APP\classes\;
 
-// use PDO;
-class user{
+namespace APP;
+
+use PDO;
+
+class user
+{
     private PDO $connection;
 
     public int $ID;
@@ -16,7 +19,8 @@ class user{
         $this->connection = $connection;
     }
 
-    public function check_admin($username): bool {
+    public function check_admin($username): bool
+    {
         $sql = 'SELECT role FROM user WHERE user_name = :username';
         $statement = $this->connection->prepare($sql);
         $statement->execute(['username' => $username]);
@@ -30,15 +34,16 @@ class user{
         }
     }
 
-    public function check_user_name($username, $user_pass): bool {
+    public function check_user_name($username, $user_pass): bool
+    {
         $sql = 'SELECT * FROM user WHERE user_name = :username';
         $statement = $this->connection->prepare($sql);
         $statement->execute([
             'username' => $username
         ]);
-    
+
         $user = $statement->fetch(PDO::FETCH_ASSOC);
-    
+
         // Kiểm tra xem có người dùng với tên người dùng đã cho không và kiểm tra mật khẩu
         if ($user !== false && $user_pass == $user['user_pass']) {
             return true; // Tên người dùng và mật khẩu khớp
@@ -46,8 +51,9 @@ class user{
             return false; // Tên người dùng không tồn tại hoặc mật khẩu không khớp
         }
     }
-    
-    public function add_user($username, $user_email, $user_pass): int {
+
+    public function add_user($username, $user_email, $user_pass): int
+    {
         // Kiểm tra xem tên người dùng đã tồn tại chưa
         if ($this->is_username_exists($username)) {
             return 0; // Tên người dùng đã tồn tại
@@ -60,10 +66,10 @@ class user{
 
         // Câu lệnh SQL để chèn một người dùng mới
         $sql = 'INSERT INTO user (user_name, user_email, user_pass) VALUES (:username, :user_email, :user_pass)';
-        
+
         // Chuẩn bị câu lệnh SQL
         $statement = $this->connection->prepare($sql);
-        
+
         // Thực thi câu lệnh SQL với các tham số đã được cung cấp
         $result = $statement->execute([
             'username' => $username,
@@ -76,7 +82,8 @@ class user{
     }
 
     // Kiểm tra xem tên người dùng đã tồn tại chưa
-    private function is_username_exists($username): bool {
+    private function is_username_exists($username): bool
+    {
         $sql = 'SELECT COUNT(*) FROM user WHERE user_name = :username';
         $statement = $this->connection->prepare($sql);
         $statement->execute(['username' => $username]);
@@ -86,7 +93,8 @@ class user{
     }
 
     // Kiểm tra xem email đã tồn tại chưa
-    private function is_email_exists($user_email): bool {
+    private function is_email_exists($user_email): bool
+    {
         $sql = 'SELECT COUNT(*) FROM user WHERE user_email = :user_email';
         $statement = $this->connection->prepare($sql);
         $statement->execute(['user_email' => $user_email]);
@@ -97,50 +105,71 @@ class user{
 
 
 
-    public function update_user($ID, $username, $user_email, $user_pass): bool {
-        // Câu lệnh SQL để cập nhật thông tin người dùng
-        $sql = 'UPDATE users SET user_name = :username, user_email = :user_email, user_pass = :user_pass WHERE ID = :ID';
-    
-        $statement = $this->connection->prepare($sql);
-    
-        // Thực thi câu lệnh SQL với các tham số đã được cung cấp
-        $result = $statement->execute([
-            'ID' => $ID,
-            'username' => $username,
-            'user_email' => $user_email,
-            'user_pass' => $user_pass
-        ]);
-    
-        // Kiểm tra xem cập nhật có thành công không
-        return $result;
-    }
-    
+    public function update_user($ID, $username, $user_email, $old_pass, $new_pass): bool
+    {
+        // Kiểm tra mật khẩu cũ trước khi cập nhật
+        if ($this->check_old_password($ID, $old_pass)) {
+            // Câu lệnh SQL để cập nhật thông tin người dùng
+            $sql = 'UPDATE user SET user_name = :username, user_email = :user_email, user_pass = :new_pass WHERE ID = :ID';
 
-    public function user_all(): array {
+            $statement = $this->connection->prepare($sql);
+
+            $statement->bindParam(':ID', $ID, PDO::PARAM_INT);
+            $statement->bindParam(':username', $username, PDO::PARAM_STR);
+            $statement->bindParam(':user_email', $user_email, PDO::PARAM_STR);
+            $statement->bindParam(':new_pass', $new_pass, PDO::PARAM_STR);
+
+            // Thực thi câu lệnh SQL và kiểm tra kết quả
+            $result = $statement->execute();
+
+            // Kiểm tra xem cập nhật có thành công không
+            return $result;
+        } else {
+            // Mật khẩu cũ không khớp
+            return false;
+        }
+    }
+
+    private function check_old_password($ID, $old_pass): bool
+    {
+        // Lấy mật khẩu cũ từ database
+        $sql = 'SELECT user_pass FROM user WHERE ID = :ID';
+        $statement = $this->connection->prepare($sql);
+        $statement->bindParam(':ID', $ID, PDO::PARAM_INT);
+        $statement->execute();
+        $row = $statement->fetch();
+
+        // Kiểm tra mật khẩu cũ
+        return $old_pass == $row['user_pass'];
+    }
+
+
+    public function user_all(): array
+    {
         $sql = 'SELECT * FROM user';
         $statement = $this->connection->prepare($sql);
         $statement->execute();
-    
+
         $users = [];
-        while($row = $statement->fetch()){
+        while ($row = $statement->fetch()) {
             $user = new user($this->connection);
             $user->ID = $row['id'];
             $user->user_email = $row['user_email'];
             $user->username = $row['user_name'];
             $user->role = $row['role'];
             $users[] = $user;
-
         };
-    
+
         return $users;
     }
-    public function user_theo_ID(int $ID_user): user {
+    public function user_theo_ID(int $ID_user): user
+    {
         $sql = 'SELECT * FROM user WHERE id = :ID_user';
         $statement = $this->connection->prepare($sql);
         $statement->execute([
             'ID_user' => $ID_user
         ]);
-    
+
         $row = $statement->fetch();
         $user = new user($this->connection);
         $user->ID = $row['id'];
@@ -148,22 +177,19 @@ class user{
         $user->username = $row['user_name'];
         $user->role = $row['role'];
 
-    
+
         return $user;
     }
 
-    public function delete($user_id): bool {
-        $sql = 'DELETE FROM user WHERE user_id = :user_id';
+    public function delete($user_id): bool
+    {
+        $sql = 'DELETE FROM user WHERE id = :user_id';
         $statement = $this->connection->prepare($sql);
-        
+
         // Bind giá trị của user_id và thực thi câu lệnh SQL
         $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
         $result = $statement->execute();
-    
+
         return $result;
     }
-    
-    
 }
-
-?>
